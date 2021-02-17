bbb2c65c6ffe3fe71e80375ab85aa69a1e245bb7
Zareix <raphcatarino@gmail.com>
2021-02-17 19:18:31 +0100

    format

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -1,17 +1,15 @@
 package serveur;
 
-
 import java.io.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.net.*;
 
-
 public class Serveur implements Runnable {
-	
+
 	private ServerSocket listen_socket;
 	private Class<? extends Runnable> service;
-	
+
 	public Serveur(int port, Class<? extends Runnable> serviceClass) throws IOException {
 		listen_socket = new ServerSocket(port);
 		this.service = serviceClass;
@@ -20,17 +18,22 @@ public class Serveur implements Runnable {
 	public void run() {
 		try {
 			Constructor<? extends Runnable> c = this.service.getConstructor(Socket.class);
-			while(true)
+			while (true)
 				new Thread((Runnable) c.newInstance(this.listen_socket.accept())).start();
-				
-		}
-		catch (IOException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			try {this.listen_socket.close();} catch (IOException e1) {}
-			System.err.println("Pb sur le port d'Ã©coute :" + e);
+		} catch (IOException | NoSuchMethodException | SecurityException | InstantiationException
+				| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			try {
+				this.listen_socket.close();
+			} catch (IOException e1) {
+			}
+			System.err.println("Pb sur le port d'écoute :" + e);
 		}
 	}
 
 	protected void finalize() throws Throwable {
-		try {this.listen_socket.close();} catch (IOException e1) {}
+		try {
+			this.listen_socket.close();
+		} catch (IOException e1) {
+		}
 	}
 }
